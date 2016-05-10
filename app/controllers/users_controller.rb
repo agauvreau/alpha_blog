06c98911668e35fd34628a135117ce7cb121b0fb
Adam Gauvreau <adamgauvreau@hotmail.com>
2016-05-10 14:52:26 +0000

    added gravatar and created user created article show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,9 @@ class UsersController < ApplicationController
     
     end
     
+    def show
+        @user = User.find(params[:id])
+    end
     
     private
     def user_params #this whitelists the parameters required to create a user. 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    #this is for the gravatar helper it uses a MD% hash that need to be transformed so Rails can use it.
+    def gravatar_for(user, options = { size: 80})
+        
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.username, class: "img-circle")
+    end
 end
